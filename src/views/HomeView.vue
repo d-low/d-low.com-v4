2a7a798a460cbc63f903a9865158943a06da6e00
Mike DiLorenzo <mikedilorenzo@gmail.com>
2023-12-03 18:36:03 -0500

    Use content store in HomeView to render links to site content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup>
-// TODO: Anything to do here?
+import { RouterLink } from 'vue-router';
+import { useContentStore } from '@/stores/content';
+
+const store = useContentStore();
+const links = store.getLinks({ path: '/', includeMostRecent: true });
 </script>
 
 <template>
   <main>
-    <p><strong>TODO:</strong>Render D-Low.com &lt;HomePage&gt; component</p>
+    <ul>
+      <li
+        v-for="link in links"
+        :key="link.href"
+      >
+        <RouterLink :to="link.href">
+          <img
+            :src="link.image"
+            width="300"
+          >
+          <strong style="display: block;">
+            {{ link.name }}
+          </strong>
+        </RouterLink>
+      </li>
+    </ul>
   </main>
 </template>
