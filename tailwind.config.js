1e0617e92973c995d47b8a3824f9cbc0ac7d5742
Mike DiLorenzo <mikedilorenzo@gmail.com>
2023-12-06 22:57:08 -0500

    Configure content sources
    SEE: https://tailwindcss.com/docs/upgrade-guide#configure-content-sources

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,13 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 
   plugins: [],
 
   // Prefix tailwind class names with "tw-" to reduce conflicts
   // SEE: https://tailwindcss.com/docs/configuration#prefix
   prefix: 'tw-',
-
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 
   theme: {
     extend: {},
