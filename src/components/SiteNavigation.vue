dfb745c3a1af97d02971dfe2f951ac5a77eaab21
Mike DiLorenzo <mikedilorenzo@gmail.com>
2023-12-07 22:43:21 -0500

    Add initial <SiteNavigation> component and conditionally render in <App>

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import { useContentStore } from '@/stores/content';
+
+const store = useContentStore();
+const links = store.getLinks({ path: '/' });
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li
+        v-for="link in links"
+        :key="link.href"
+      >
+        <RouterLink
+          :to="link.href"
+        >
+          {{ link.name  }}
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
