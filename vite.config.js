1d481a0b77c1062cd5c1997fcde3f4d90176e10f
Mike DiLorenzo <mikedilorenzo@gmail.com>
2023-12-07 22:34:08 -0500

    Enable source maps in vite bulid

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true
+  },
   plugins: [
     vue(),
     eslintPlugin(),
