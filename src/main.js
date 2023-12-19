477d9f55dd223e5305eb0f787a5c5a94b5bd718c
Mike DiLorenzo <mikedilorenzo@gmail.com>
2023-12-18 22:20:27 -0500

    Add FontAwesome to project

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
-import './assets/styles/main.css'
+import './assets/styles/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faXTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-import App from './App.vue'
-import router from './router'
+library.add(faEnvelope, faFacebook, faGithub, faLinkedin, faXTwitter);
 
-const app = createApp(App)
+import App from './App.vue';
+import router from './router';
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app');
