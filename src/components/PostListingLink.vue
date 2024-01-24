512d17162282874c56860cb4e7845bb64f4ea816
Mike DiLorenzo <mikedilorenzo@gmail.com>
2024-01-23 22:29:59 -0500

    WIP: Add initial <PostListingLink> component and render in Post Listing View

---
+++ src/components/PostListingLink.vue
@@ -0,0 +1,63 @@
+<script setup>
+/**
+ * @todo
+ * - Image preview layout (2/3 width hero with stacked images to left/right in remaining 1/3)
+ * - View All +X more link
+ * - Display cropped text
+ * - Read More/Read Less link to show/hide cropped text
+ * - Image viewer (Use FancyBox? https://fancyapps.com/resources/integration/#vue)
+ */
+import CardContainer from '@/components/CardContainer.vue';
+import ImageLazyFade from '@/components/ImageLazyFade.vue';
+
+const props = defineProps({
+  postListingLink: {
+    type: Object,
+    required: true,
+  },
+  reverseLayout: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const images = props.postListingLink.images.slice(0, 4);
+
+const titleClass = [
+  'tw-text-center',
+  'tw-text-3xl lg:tw-text-4xl',
+  'font-passion-one-regular',
+];
+
+const dateClass = [
+  'tw-mb-5 lg:tw-mb-6',
+  'tw-text-center',
+  'tw-text-2xl lg:tw-text-3xl',
+  'font-passion-one-regular',
+];
+</script>
+
+<template>
+  <CardContainer>
+    <header>
+      <h3 :class="titleClass">
+        {{ postListingLink.name }}
+      </h3>
+      <h4 :class="dateClass">
+        {{ postListingLink.date }}
+      </h4>
+    </header>
+    <div>
+      <ul>
+        <li
+          v-for="image in images"
+          :key="image"
+        >
+          <a href="javascript:void(0);">
+            <ImageLazyFade :src="image" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </CardContainer>
+</template>
--- src/views/PostListingView.vue
+++ src/views/PostListingView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { useRoute } from 'vue-router';
 import { useContentStore } from '@/stores/content';
-import CardContainer from '@/components/CardContainer.vue';
+import PageTitle from '@/components/PageTitle.vue';
+import PostListingLink from '@/components/PostListingLink.vue';
+import SiteHeader from '@/components/SiteHeader.vue';
 
 const route = useRoute();
 const store = useContentStore();
@@ -11,16 +13,18 @@ const postListingLinks = store.getPostListingLinks(path);
 </script>
 
 <template>
+  <SiteHeader />
+  <PageTitle />
   <ul class="tw-w-full tw-pt-4 tw-px-2 lg:tw-px-4 tw-pb-8">
     <li
-      v-for="postListingLink in postListingLinks"
+      v-for="(postListingLink, index) in postListingLinks"
       :key="postListingLink.href"
-      class="tw-mb-4 lg:tw-mb-20"
+      class="lg:tw-max-w-4xl lg:tw-mx-auto tw-mb-4 lg:tw-mb-20"
     >
-      <CardContainer>
-        <h3>{{ postListingLink.name }}</h3>
-        <h4>{{ postListingLink.date }}</h4>
-      </CardContainer>
+      <PostListingLink
+        :post-listing-link="postListingLink"
+        :reverse-layout="index % 2 === 1"
+      />
     </li>
   </ul>
 </template>
